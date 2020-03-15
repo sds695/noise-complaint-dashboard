dd19e003ad3846da78860e5212fb01195fce5011
sds695 <sds695@nyu.edu>
2020-03-15 12:07:02 -0400

    Initial app boilerplate

--- app.py
+++ app.py
@@ -18,6 +18,21 @@ app = dash.Dash(__name__)
 server = app.server
 app.title = 'NYC 311 construction complaints'
 
+#Blueprint error
+from flask import Blueprint
+
+# a random blueprint
+myblueprint = Blueprint(name='mycustomblueprint',
+                         import_name=__name__, # name of the file
+                         static_folder='static', # a folder inside app/myblueprint/static
+                         template_folder='templates', # a folder inside app/myblueprint/templates
+                         static_url_path='/static', # this is what mycustomblueprint.static will point to, and if the name is admin it will be admin.static, thus colliding with flask-admin
+                         url_prefix='/myblueprintprefix', # this will be appended to each view inside your blueprint, i.e. a view '/foo' will get converted into '/myblueprintprefix/foo' in your url mappings
+                         subdomain=None,
+                         url_defaults=None,
+                         root_path=None)
+#Close block
+
 layout_map = dict(
     autosize=True,
     height=500,
