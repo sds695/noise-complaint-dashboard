71bd6454d7e861176861b3c367139d939012d137
sds695 <sds695@nyu.edu>
2020-03-15 14:43:57 -0400

    Initial app boilerplate

--- myblueprint.py
+++ myblueprint.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 # a random blueprint
 myblueprint = Blueprint(name='mycustomblueprint',
-                         import_name=__name__, # name of the file
+                         import_name='blueprint_name', # name of the file
                          static_folder='static', # a folder inside app/myblueprint/static
                          template_folder='templates', # a folder inside app/myblueprint/templates
                          static_url_path='/static', # this is what mycustomblueprint.static will point to, and if the name is admin it will be admin.static, thus colliding with flask-admin
