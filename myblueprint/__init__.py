405b70748c20a3fea8263bf9f7a6d6fdd4cdff07
sds695 <sds695@nyu.edu>
2020-03-15 12:27:43 -0400

    Initial app boilerplate

---
+++ myblueprint/__init__.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+# a random blueprint
+myblueprint = Blueprint(name='mycustomblueprint',
+                         import_name=__name__, # name of the file
+                         static_folder='static', # a folder inside app/myblueprint/static
+                         template_folder='templates', # a folder inside app/myblueprint/templates
+                         static_url_path='/static', # this is what mycustomblueprint.static will point to, and if the name is admin it will be admin.static, thus colliding with flask-admin
+                         url_prefix='/myblueprintprefix', # this will be appended to each view inside your blueprint, i.e. a view '/foo' will get converted into '/myblueprintprefix/foo' in your url mappings
+                         subdomain=None,
+                         url_defaults=None,
+                         root_path=None)
+
